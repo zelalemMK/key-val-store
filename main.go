33eca6bba4a715686d3d75f1c7463964d44882fb
Zelalem Mekonnen <zelliott33@gmail.com>
2023-06-26 15:52:23 -0500

    gracefull shutdown and save data at the end of the program

--- main.go
+++ main.go
@@ -1,23 +1,31 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"path/filepath"
+	"syscall"
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
 )
 
-var serverPort string
-
-var key_val = make(map[string]string) // for now, we will handle complex values
+var (
+	serverPort  string
+	StoragePath = "/tmp/keyVal"
+	key_val     = make(map[string]string) // for now, we will handle complex values
+)
 
 const url = "http://localhost"
 
 func main() {
+	// defer fmt.Println("Program terminated")
+	// defer saveFile(key_val)
 	serverPort = "8080" // if env port is not set
 
 	if fromEnv := os.Getenv("PORT"); fromEnv != "" {
@@ -38,9 +46,30 @@ func main() {
 
 	// get port from env
 
-	err := http.ListenAndServe(":"+serverPort, r)
-	if err != nil {
-		log.Fatal("Error running the server")
+	go func() {
+		err := http.ListenAndServe(":"+serverPort, r)
+		if err != nil {
+			log.Fatal("Error running the server")
+		}
+	}()
+
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt, syscall.SIGTERM)
+
+	go func() {
+		<-signalChan
+		fmt.Println("Received an interrupt, stopping services...")
+		saveFile(key_val)
+		os.Exit(0)
+	}()
+
+	// when the program ends, save the varliable into a file/localdisc as json
+	//check if program has been terminated
+	// worry about if the server
+
+	// https://us04web.zoom.us/j/73294374645?pwd=PajErocVbvTQFI0mnJYiAm4osZanDj.1
+	// Koy ande. OK
+	for {
 	}
 
 }
@@ -49,7 +78,6 @@ func home(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello World"))
 }
 
-// r.Get("/key/{key}")
 func Get(w http.ResponseWriter, r *http.Request) {
 	key := chi.URLParam(r, "key")
 	value, ok := key_val[key]
@@ -78,4 +106,34 @@ func Set(w http.ResponseWriter, r *http.Request) {
 func Delete(w http.ResponseWriter, r *http.Request) {
 	key := chi.URLParam(r, "key")
 	delete(key_val, key)
+}
+
+func saveFile(kv map[string]string) {
+	// First check if the folder exists and create it if it is missing.
+	if _, err := os.Stat(StoragePath); os.IsNotExist(err) {
+		err = os.MkdirAll(StoragePath, 0755)
+		if err != nil {
+			log.Fatalf("Failed to create storage path: %v", err)
+		}
+	}
+
+	var f *os.File
+	if _, err := os.Stat(filepath.Join(StoragePath, "keyVal.json")); os.IsNotExist(err) {
+		f, err = os.Create(StoragePath + "/keyVal.json")
+		if err != nil {
+			log.Fatalf("Failed to create storage path: %v", err)
+		}
+
+	}
+	defer f.Close()
+
+	//encode the map int json
+	//write the json into the file
+	encodedData, err := json.Marshal(kv)
+	if err != nil {
+		log.Fatalf("Failed to encode data: %v", err)
+	}
+
+	f.Write(encodedData)
+
 }
